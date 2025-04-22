1d7afb5e3796715afbb0b0493318e4f7964e970b
Jagveer Kaur Khalsa <144109408+jagv091205@users.noreply.github.com>
2025-04-22 16:30:35 +0530

    Update config.js

--- inventory-management/src/firebase/config.js
+++ inventory-management/src/firebase/config.js
@@ -13,14 +13,7 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA5jVQcxmKKLQy31Ft8329EB_cWRyBu0uI",
-    authDomain: "bhookiecore.firebaseapp.com",
-    databaseURL: "https://bhookiecore-default-rtdb.firebaseio.com",
-    projectId: "bhookiecore",
-    storageBucket: "bhookiecore.firebasestorage.app",
-    messagingSenderId: "252666746613",
-    appId: "1:252666746613:web:e97a48d4c31c1d952d17bc",
-    measurementId: "G-STVRJ77V79"
+    //API KEY
   };
 
 // Initialize Firebase
